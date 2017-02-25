9ea2cb814289812c35718d6210377fc46c60114d
vagrant <vagrant@homestead>
2017-02-25 14:43:43 +0000

    Add Category model, migrations & seeder.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Area;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -36,6 +36,26 @@ class AppServiceProvider extends ServiceProvider
             $area->slug = str_slug($prefix . $area->name);
         });
 
+        // On Category create
+        Category::creating(function ($category) {
+            // Init prefix
+            $prefix = '';
+
+            // Init parent.
+            $parent = $category->parent;
+
+            // Iterate through all parents until root node
+            while ($parent) {
+                // Prepend to prefix
+                $prefix = $parent->name . ' ' . $prefix;
+
+                // Move to next parent
+                $parent = $parent->parent;
+            }
+
+            // generate slug prefix + area name
+            $category->slug = str_slug($prefix . $category->name);
+        });
     }
 
     /**
